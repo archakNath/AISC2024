68136c285609b023ab56ccb6ec16fd98d97bbafa
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-02-03 02:05:04 +0530

    add footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const indi_screens = document.querySelectorAll("section.display .screen .indi-screen");
 const menu_btns = document.querySelectorAll("section.display .menu li");
+const footer_links = document.querySelectorAll("footer a");
+const download_button = document.getElementById("download");
 
 for (let i = 0; i < menu_btns.length; i++) {
     const button = menu_btns[i];
@@ -15,4 +17,23 @@ for (let i = 0; i < menu_btns.length; i++) {
         menu_btns[i].setAttribute("class", "active");
         indi_screens[i].style.display = "block";
     }
+}
+for (let i = 0; i < footer_links.length; i++) {
+    const button = footer_links[i];
+    button.onclick = () => {
+        for (let j = 0; j < indi_screens.length; j++) {
+            const screen = indi_screens[j];
+            screen.style.display = "none";
+        }
+        for (let j = 0; j < menu_btns.length; j++) {
+            const button = menu_btns[j];
+            button.removeAttribute("class");
+        }
+        menu_btns[i].setAttribute("class", "active");
+        indi_screens[i].style.display = "block";
+    }
+}
+
+download_button.onclick = () => {
+    window.open("https://firebasestorage.googleapis.com/v0/b/artco-bb7b5.appspot.com/o/AISC2024-CFP.pdf?alt=media&token=17b4f150-ce62-4553-80d9-3a30f865c02b", "_blank")
 }
