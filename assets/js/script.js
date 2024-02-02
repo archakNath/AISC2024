8baf3cd8aa324a0d43e549c2f490488e5e80814f
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-02-03 02:10:32 +0530

    made scroll using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const indi_screens = document.querySelectorAll("section.display .screen .indi-screen");
 const menu_btns = document.querySelectorAll("section.display .menu li");
-const footer_links = document.querySelectorAll("footer a");
+const footer_links = document.querySelectorAll("footer li");
 const download_button = document.getElementById("download");
 
 for (let i = 0; i < menu_btns.length; i++) {
@@ -31,9 +31,15 @@ for (let i = 0; i < footer_links.length; i++) {
         }
         menu_btns[i].setAttribute("class", "active");
         indi_screens[i].style.display = "block";
+        scrollToSection();
     }
 }
 
 download_button.onclick = () => {
     window.open("https://firebasestorage.googleapis.com/v0/b/artco-bb7b5.appspot.com/o/AISC2024-CFP.pdf?alt=media&token=17b4f150-ce62-4553-80d9-3a30f865c02b", "_blank")
 }
+
+function scrollToSection() {
+    var targetSection = document.getElementById('display');
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
